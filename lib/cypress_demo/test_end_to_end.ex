0d6f237136bd2717041581b0b9018569fa3b0f76
Maxim Filimonov <tpaktopsp@gmail.com>
2021-04-14 17:17:19 +0800

    add checkin and checkout methods

--- lib/cypress_demo/test_end_to_end.ex
+++ lib/cypress_demo/test_end_to_end.ex
@@ -4,5 +4,41 @@ defmodule CypressDemo.Plug.TestEndToEnd do
   plug :match
   plug :dispatch
 
+  post "/db/checkout" do
+    # If the agent is registered and alive, a db connection is checked out already
+    # Otherwise, we spawn the agent and let it(!) check out the db connection
+    owner_process = Process.whereis(:db_owner_agent)
+
+    if owner_process && Process.alive?(owner_process) do
+      send_resp(conn, 200, "connection has already been checked out")
+    else
+      {:ok, _pid} = Agent.start_link(&checkout_shared_db_conn/0, name: :db_owner_agent)
+      send_resp(conn, 200, "checked out database connection")
+    end
+  end
+
+  post "/db/checkin" do
+    # If the agent is registered and alive, we check the connection back in
+    # Otherwise, no connection has been checked out, we ignore this
+    owner_process = Process.whereis(:db_owner_agent)
+
+    if owner_process && Process.alive?(owner_process) do
+      Agent.get(owner_process, &checkin_shared_db_conn/1)
+      Agent.stop(owner_process)
+      send_resp(conn, 200, "checked in database connection")
+    else
+      send_resp(conn, 200, "connection has already been checked back in")
+    end
+  end
+
   match(_, do: send_resp(conn, 404, "not found"))
+
+  defp checkout_shared_db_conn do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkout(CypressDemo.Repo, ownership_timeout: :infinity)
+    :ok = Ecto.Adapters.SQL.Sandbox.mode(CypressDemo.Repo, {:shared, self()})
+  end
+
+  defp checkin_shared_db_conn(_) do
+    :ok = Ecto.Adapters.SQL.Sandbox.checkin(CypressDemo.Repo)
+  end
 end
