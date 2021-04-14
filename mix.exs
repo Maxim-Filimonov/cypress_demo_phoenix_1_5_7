3bd5e1ec341866928e6acefe87c94a3d26c93527
Maxim Filimonov <tpaktopsp@gmail.com>
2021-04-14 17:31:21 +0800

    add poison - needed for example code

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule CypressDemo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:ex_machina, "~> 2.7.0"}
+      {:ex_machina, "~> 2.7.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 
